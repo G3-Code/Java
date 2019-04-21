a5b1771ba083aa45a0c5ba36589216e5a9e78b01
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-04-21 11:12:19 -0500

    Testing the files for git update

--- helloapp/src/hello/Greeter.java
+++ helloapp/src/hello/Greeter.java
@@ -2,7 +2,12 @@ package hello;
 
 // This is a test message
 public class Greeter{
+    int count = 0;
     public String sayHello(){
         return "Hello World!!";
+    }
+
+    public int getCount(){
+        return this.count;
     }
 }
